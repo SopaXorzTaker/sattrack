72eb30606276152111d8d6214a207979bc2349f6
SopaXorzTaker <markboldyrev@gmail.com>
2017-10-07 12:36:05 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sattrack',
+    version='0.0.1',
+    packages=['sattrack'],
+    url='https://github.com/SopaXorzTaker/sattrack',
+    license='',
+    author='SopaXorzTaker',
+    author_email='markboldyrev@gmail.com',
+    description='A simple satellite pass predictor'
+)
