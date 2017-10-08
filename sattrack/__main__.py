e537bd61049497016730fa840628736d7503d109
SopaXorzTaker <markboldyrev@gmail.com>
2017-10-08 15:38:44 +0300

    Allow calculating the position without coordinates, fix utils

--- sattrack/__main__.py
+++ sattrack/__main__.py
@@ -9,7 +9,8 @@ from sattrack.coordinates import *
 
 parser = argparse.ArgumentParser("sattracker")
 parser.add_argument("tle", help="The TLE file to use")
-parser.add_argument("coords", help="The coordinates (lat, lon, alt) of the observer")
+parser.add_argument("-c", "--coords", help="The coordinates (lat, lon, alt) of the observer to calculate the"
+                                           " azimuth and elevation", default=None)
 parser.add_argument("-d", "--duration", help="Days into the future to predict passes", type=int, default=14)
 parser.add_argument("-s", "--step", help="The step for calculation of the orbit in seconds", type=int, default=30)
 parser.add_argument("-e", "--elevation", help="Only display passes with the elevation higher than this value",
@@ -17,8 +18,12 @@ parser.add_argument("-e", "--elevation", help="Only display passes with the elev
 parser.add_argument("-n", "--satellite", help="If there are multiple satellites, use the first one named", default="")
 
 args = parser.parse_args()
-lat, lon, alt = args.coords.split(",")
-lat, lon, alt = float(lat), float(lon), float(alt)
+
+coords = args.coords
+lat, lon, alt = None, None, None
+if coords:
+    lat, lon, alt = coords.split(",")
+    lat, lon, alt = float(lat), float(lon), float(alt)
 
 with open(args.tle, "r") as tle_file:
     tle_lines = tle_file.readlines()
@@ -45,7 +50,8 @@ if not tle:
 if satellite_name:
     print("Predictions for satellite %s" % satellite_name)
 
-print("Station location: %8f %9f" % (lat, lon))
+if coords:
+    print("Station location: %8f %9f" % (lat, lon))
 
 orbital_elements = None
 try:
@@ -60,8 +66,10 @@ jd = jd_now()
 local = tz.tzlocal()
 seconds_since_epoch = (jd - orbital_elements.get_epoch_jd()) * DAY
 
+max_el = None
 current_pass = False
-max_el = 0
+az, el, rg = None, None, None
+
 for i in range(0, int(DAY * args.duration / args.step)):
     theta = jd_theta(jd)
     timestamp = jd_to_date(jd).astimezone(local).strftime("%Y-%m-%d %H:%M:%S")
@@ -71,23 +79,30 @@ for i in range(0, int(DAY * args.duration / args.step)):
         print("The satellite decayed at %s" % timestamp)
 
     ecef = eci_to_ecef(eci, theta)
-    topo = eci_to_topo(eci, (lat, lon, alt), theta)
+
+    if coords:
+        topo = eci_to_topo(eci, (lat, lon, alt), theta)
+        az, el, rg = topo
+
     lla = ecef_to_lla(ecef)
-    az, el, rg = topo
-    if el > args.elevation:
+    if not coords or el > args.elevation:
         if not current_pass:
             print("Pass at %s" % timestamp)
             print("Timestamp           Lat      Lon        Alt     Az     El   Rng")
             current_pass = True
             max_el = 0
 
-        if el > max_el:
+        if coords and el > max_el:
             max_el = el
 
-        print("%s %8.4f %9.4f %8.0f %6.1f %4.1f %7.0f" % (timestamp, *lla, *topo))
+        if coords:
+            print("%s %8.4f %9.4f %8.0f %6.1f %4.1f %7.0f" % (timestamp, *lla, *topo))
+        else:
+            print("%s %8.4f %9.4f %8.0f" % (timestamp, *lla))
     elif current_pass:
-        print("Maximum elevation: %.1f" % max_el)
-        print("\n")
+        if coords:
+            print("Maximum elevation: %.1f" % max_el)
+            print("\n")
         current_pass = False
 
     jd += args.step / DAY
--- sattrack/utils.py
+++ sattrack/utils.py
@@ -7,19 +7,39 @@ from sattrack.geodesy import DAY
 
 JD2000_EPOCH = datetime.datetime(2000, 1, 1, 12, 0, 0)
 
+
 def jd_now():
+    """
+    Computes the current Julian date.
+    :return: the current Julian date
+    """
     utc_now = datetime.datetime.utcnow()
     delta = utc_now - JD2000_EPOCH
     jd = 2451545.0 + delta.days + delta.seconds / DAY
 
     return jd
 
+
 def jd_to_date(jd):
+    """
+    Converts a Julian date to a datetime object.
+    :param jd: a Julian date
+    :return: a datetime object corresponding to the date given
+    """
     delta = jd - 2451545.0
     date = (JD2000_EPOCH + datetime.timedelta(delta)).replace(tzinfo=tz.tzutc())
     return date
 
 
 def epoch_to_jd(epoch_year, epoch):
+    """
+    Converts a TLE epoch to a Julian date.
+    :param epoch_year: the epoch year
+    :param epoch: the epoch day
+    :return: the computed Julian date
+    """
+    if epoch_year > 57:
+        epoch_year -= 100
+
     date = 2451544.5 + epoch_year*365 + int((epoch_year-1)/4) + epoch
     return date
